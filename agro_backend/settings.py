02e6991a99132e11ee9026e9d4860cb68cdbe81f
brauliomendoza-debug <braulio.mendoza@tecsup.edu.pe>
2026-06-03 14:58:21 -0500

    arreglando conexion

--- agro_backend/settings.py
+++ agro_backend/settings.py
@@ -23,9 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m$nird*)l)t8clzdnjj++(3b=0j84sk8m$-*uquk$kes^(8mb3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "backend-agro-be61.onrender.com",
+    "localhost",
+    "127.0.0.1"
+]
 
 
 # Application definition
@@ -132,4 +136,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "https://frontend-agro-m1uq.onrender.com"
+]
+CORS_ALLOW_CREDENTIALS = True
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://frontend-agro-m1uq.onrender.com"
+]
+
